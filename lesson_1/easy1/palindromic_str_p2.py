cf57d0e3c728fafd69c3b70fa7cae12d6437f1a7
Albert <10779212+alberthwang@users.noreply.github.com>
2024-11-12 00:04:04 -0800

    palindrome string p1 and start of p2

---
+++ lesson_1/easy1/palindromic_str_p2.py
@@ -0,0 +1,10 @@
+'''
+alindromic Strings (Part 2)
+
+Write another function that returns True if the string passed as an argument is a palindrome, or False otherwise. 
+This time, however, your function should be case-insensitive, and should ignore all non-alphanumeric characters.
+If you wish, you may simplify things by calling the is_palindrome function you wrote in the previous exercise.
+'''
+
+def is_palindrome(word):
+  pass
