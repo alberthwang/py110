5aa8470279129b090f1ace865445ac1925da121a
alberthwang <10779212+alberthwang@users.noreply.github.com>
2025-04-15 20:49:12 -0700

    board and display board changes

--- lesson_3/tictactoe.py
+++ lesson_3/tictactoe.py
@@ -1,18 +1,28 @@
 
 
-def displayBoard():
+def displayBoard(board):
     print('')
     print('     |     |')
-    print('     |     |')
+    print(f'  {board[1]}  |  {board[2]}  |   {board[3]}')
     print('     |     |')
     print('-----+-----+-----')
     print('     |     |')
-    print('     |     |')
+    print(f'  {board[4]}  |  {board[5]}  |   {board[6]}')
     print('     |     |')
     print('-----+-----+-----')
     print('     |     |')
-    print('     |     |')
-    print('     |     |')
+    print(f'  {board[7]}  |  {board[8]}  |  {board[9]}')
     print('     |     |')
     print('')
-    
+
+
+board = {1 : 'X',
+         2 : ' ',
+         3 : ' ',
+         4 : 'O',
+         5 : ' ',
+         6 : ' ',
+         7 : ' ',
+         8 : ' ',
+         9 : ' '}
+displayBoard(board)
