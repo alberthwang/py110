dd5b93e9db7d0dda0740159e3ecbe8ebe6a31612
Albert <10779212+alberthwang@users.noreply.github.com>
2025-08-15 14:16:56 -0700

    start new assignment 21

---
+++ lesson_3/twentyone.py
@@ -0,0 +1,16 @@
+'''
+assignment : 21
+52 card deck, 4 suits, 13 vals(2,3,4,5,6,7,8,9,10, j,q,k,a)
+dont go over 21 or yo ulose
+
+2-10: face value, j/q/k:10, a: 1 or 11
+
+player hits until stay or bust
+dealer hits until 17+
+see who has closest to 21
+
+
+'''
+
+def play_game():
+  pass
