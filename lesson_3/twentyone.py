991d08efd3cabc995774d8b56034ba36ab274b4e
Albert <10779212+alberthwang@users.noreply.github.com>
2026-01-07 23:43:42 -0800

    implmented deal cards or start of game

--- lesson_3/twentyone.py
+++ lesson_3/twentyone.py
@@ -27,23 +27,35 @@ def shuffle(deck):
     
 
 def initiate_deck():
-  values = [i for i in range(1,14)]
-  suits = ['clubs', 'diamond' , 'heart', 'spade']
-  cards = [f"{suit}, {value}" for suit in suits for value in values]
+  values = ['2', '3', '4', '5','6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  suits = ['C', 'D', 'H', 'S']
+  cards = [[suit, value] for suit in suits for value in values]
   #print(cards)
   #deck = {i: [suits[(i- 1) // 13], (i % 13) + 1] for i in range(1,53)}
-  deck = [[card] for card in cards]
+  deck = [card for card in cards]
   #print(deck)
   return deck
   
 def draw_card(deck):
   return deck.pop()
 
+def deal_cards(deck):
+  hand, opponent_hand  = [],[]
+  hand.append(draw_card(deck))
+  hand.append(draw_card(deck))
+  
+  opponent_hand.append(draw_card(deck))
+  opponent_hand.append(draw_card(deck))
+  
+  print( hand, opponent_hand)
+  
+  return 
 
 def play_game():
   deck = initiate_deck()
   shuffle(deck)
-  print(deck)
-  print(draw_card(deck))
+  deal_cards(deck)
+  #print(deck)
+  #print(draw_card(deck))
 
 play_game() 
